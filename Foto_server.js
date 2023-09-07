611775774b6c1b64556dea7b8d1b278e1d944ee2
OliverN <137147421+OliverNagl@users.noreply.github.com>
2023-09-07 19:26:49 +0200

    Deploy foto server

--- Foto_server.js
+++ Foto_server.js
@@ -1,61 +1,48 @@
 const express = require('express');
+const AWS = require('aws-sdk');
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware to parse JSON data
 app.use(express.json());
 
+// Configure AWS SDK with your Access Key ID and Secret Access Key
+AWS.config.update({
+  accessKeyId: 'AKIAVZH4SBSY6M4YGRUO',
+  secretAccessKey: 'wg12E96XuenOao/XGmWfY0bzBjl8Ag+yEv9XzzGx'
+});
+
+// Create a new instance of the S3 service
+const s3 = new AWS.S3();
+
 // Endpoint to handle photo upload
 app.post('/upload', (req, res) => {
   const photoData = req.body.photo;
-  // Process the photo data as needed (e.g., save it to a file, store it in a database, etc.)
-  // Replace the following code with your own logic
+  const base64Data = photoData.replace(/^data:image\/\w+;base64,/, '');
+  const buffer = Buffer.from(base64Data, 'base64');
 
-  // Example response
-  res.status(200).json({ message: 'Photo uploaded successfully!' });
+  // Set the parameters for the S3 upload
+  const params = {
+    Bucket: 'bucketeer-0b6051cb-e249-4102-87b3-8fba714cf40e',
+    Key: `photos/photo_${Date.now()}.png`,
+    Body: buffer,
+    ContentEncoding: 'base64',
+    ContentType: 'image/png'
+  };
+
+  // Upload the photo to Bucketeer
+  s3.upload(params, (err, data) => {
+    if (err) {
+      console.error('Error uploading photo to Bucketeer:', err);
+      res.status(500).json({ error: 'Failed to upload photo' });
+    } else {
+      console.log('Photo uploaded to Bucketeer:', data.Location);
+      res.status(200).json({ message: 'Photo uploaded successfully' });
+    }
+  });
 });
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-const { MongoClient } = require('mongodb');
-
-// Connection URL and database name
-const url = 'mongodb://localhost:27017';
-const dbName = 'your-database-name';
-
-// Create a new MongoClient
-const client = new MongoClient(url);
-
-// Connect to the MongoDB server
-client.connect(function(err) {
-  if (err) {
-    console.error('Error connecting to the database:', err);
-  } else {
-    console.log('Connected to the database');
-
-    // Database instance
-    const db = client.db(dbName);
-
-    // Use the database for further operations
-    // ...
-  }
-});
-
-app.post('/upload', (req, res) => {
-    const photoData = req.body.photo;
-
-    // Store the photo in the database
-    const collection = db.collection('photos');
-    collection.insertOne({ photo: photoData }, function(err, result) {
-      if (err) {
-        console.error('Error storing photo in the database:', err);
-        res.status(500).json({ error: 'Failed to store photo in the database' });
-      } else {
-        console.log('Photo stored in the database');
-        res.status(200).json({ message: 'Photo uploaded and stored successfully' });
-      }
-    });
-  });
