5c460b160f69106103d4f3665a006e83d931a45f
OliverN <137147421+OliverNagl@users.noreply.github.com>
2023-09-07 19:45:54 +0200

    Test

--- Foto_server.js
+++ Foto_server.js
@@ -1,19 +1,19 @@
 const express = require('express');
-const AWS = require('aws-sdk');
 const app = express();
 const port = process.env.PORT || 3000;
+const { Pool } = require('pg');
 
 // Middleware to parse JSON data
 app.use(express.json());
 
-// Configure AWS SDK with your Access Key ID and Secret Access Key
-AWS.config.update({
-  accessKeyId: 'AKIAVZH4SBSY6M4YGRUO',
-  secretAccessKey: 'wg12E96XuenOao/XGmWfY0bzBjl8Ag+yEv9XzzGx'
+// Configure PostgreSQL connection
+const pool = new Pool({
+  host: 'ec2-52-209-225-31.eu-west-1.compute.amazonaws.com',
+  port: '5432',
+  database: 'ddous02qo0iscu',
+  user: 'zkeursujlkqvvy',
+  password: '7fc8b086ccfdda6d83912e3f70547f604cb5e7cbccfc52200f7ad62e9117b392'
 });
-
-// Create a new instance of the S3 service
-const s3 = new AWS.S3();
 
 // Endpoint to handle photo upload
 app.post('/upload', (req, res) => {
@@ -21,22 +21,13 @@ app.post('/upload', (req, res) => {
   const base64Data = photoData.replace(/^data:image\/\w+;base64,/, '');
   const buffer = Buffer.from(base64Data, 'base64');
 
-  // Set the parameters for the S3 upload
-  const params = {
-    Bucket: 'bucketeer-0b6051cb-e249-4102-87b3-8fba714cf40e',
-    Key: `photos/photo_${Date.now()}.png`,
-    Body: buffer,
-    ContentEncoding: 'base64',
-    ContentType: 'image/png'
-  };
-
-  // Upload the photo to Bucketeer
-  s3.upload(params, (err, data) => {
+  // Insert the photo into the PostgreSQL database
+  pool.query('INSERT INTO photos (photo_data) VALUES ($1)', [buffer], (err, result) => {
     if (err) {
-      console.error('Error uploading photo to Bucketeer:', err);
+      console.error('Error uploading photo to PostgreSQL:', err);
       res.status(500).json({ error: 'Failed to upload photo' });
     } else {
-      console.log('Photo uploaded to Bucketeer:', data.Location);
+      console.log('Photo uploaded to PostgreSQL');
       res.status(200).json({ message: 'Photo uploaded successfully' });
     }
   });
