02d0bb4d9e8e0db7a93c953460de956defa97539
OliverN <137147421+OliverNagl@users.noreply.github.com>
2023-09-13 14:00:58 +0100

    compile please

--- Foto_server.js
+++ Foto_server.js
@@ -15,12 +15,13 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/FotoUpload.html');
 });
 
-// Endpoint to handle photo upload
+
 app.post('/upload', (req, res) => {
   const photoData = req.body.photo;
   const base64Data = photoData.replace(/^data:image\/\w+;base64,/, '');
   const buffer = Buffer.from(base64Data, 'base64');
 
+  // Compress and convert to JPEG
   sharp(buffer)
     .jpeg({ quality: 80 })
     .toBuffer()
@@ -32,16 +33,22 @@ app.post('/upload', (req, res) => {
         ContentType: 'image/jpeg', // Set the content type accordingly
       };
 
-  s3.upload(params, (err, data) => {
-    if (err) {
-      console.error('Error uploading photo to Bucketeer:', err);
-      res.status(500).json({ error: 'Failed to upload photo' });
-    } else {
-      console.log('Photo uploaded to Bucketeer');
-      res.status(200).json({ message: 'Photo uploaded successfully' });
-    }
-  });
+      s3.upload(params, (err, data) => {
+        if (err) {
+          console.error('Error uploading photo to Bucketeer:', err);
+          res.status(500).json({ error: 'Failed to upload photo' });
+        } else {
+          console.log('Photo uploaded to Bucketeer');
+          res.status(200).json({ message: 'Photo uploaded successfully' });
+        }
+      });
+    })
+    .catch((error) => {
+      console.error('Error compressing photo:', error);
+      res.status(500).json({ error: 'Failed to compress photo' });
+    });
 });
+
 
 // Start the server
 app.listen(port, () => {
